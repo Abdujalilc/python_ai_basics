d5dc8bd1321f1ae1e890a36721f1607542540013
abdujalilc <abdujalilc@gmail.com>
2024-11-27 20:18:52 +0500

    qwer

--- chatbot/models/language_model.py
+++ chatbot/models/language_model.py
@@ -1,8 +1,23 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
-def load_language_model(pretrained_model="distilgpt2"):
+def cache_language_models(model_names):
+    for model_name in model_names:
+        print(f"Downloading and caching model: {model_name}")
+        AutoModelForCausalLM.from_pretrained(model_name)
+        AutoTokenizer.from_pretrained(model_name)
+        print(f"Cached: {model_name}")
+
+# Example usage
+model_list = [
+    "distilgpt2",
+    "gpt-neo-125M",
+    "gpt-neo-1.3B",
+    "gpt-neo-2.7B"
+]
+
+cache_language_models(model_list)
+
+def load_language_model(pretrained_model: str = "distilgpt2"):
     model = AutoModelForCausalLM.from_pretrained(pretrained_model)
     tokenizer = AutoTokenizer.from_pretrained(pretrained_model)
     return model, tokenizer
-
-lm_model, tokenizer = load_language_model()
--- chatbot/services/chatbot_logic.py
+++ chatbot/services/chatbot_logic.py
@@ -1,6 +1,6 @@
 from schemas.chat_request import ChatRequest
 from custom_faiss.faiss_manager_singleton import create_faiss_manager
-from models.language_model import lm_model, tokenizer
+from models.language_model import load_language_model
 
 def chatbot_logic(request: ChatRequest):
     faiss_manager = create_faiss_manager(request.faiss_settings.embedder_model,request.faiss_settings.distance_metric)
@@ -21,7 +21,7 @@ def chatbot_logic(request: ChatRequest):
 
     if not valid_matches:
         return "No relevant information found."
-
+    lm_model, tokenizer = load_language_model(request.transformer_settings.language_model)
     inputs = tokenizer(f"Answer briefly: {request.question}\nContext: {valid_matches[0]}", return_tensors="pt")##pt-PyTorch
     output = lm_model.generate(inputs["input_ids"], max_new_tokens=request.transformer_settings.max_new_tokens, temperature=request.transformer_settings.temperature, top_k=request.transformer_settings.top_k, top_p=request.transformer_settings.top_p)
     return tokenizer.decode(output[0], skip_special_tokens=request.transformer_settings.skip_special_tokens).split(".")[0].strip()
