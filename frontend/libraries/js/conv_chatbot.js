bc29b9eb6b8cbc85f7f01357bac8efc69112b582
abdujalilc <abdujalilc@gmail.com>
2024-11-28 16:03:56 +0500

    minor fix

--- frontend/libraries/js/conv_chatbot.js
+++ frontend/libraries/js/conv_chatbot.js
@@ -1,4 +1,5 @@
-const apiUrl = "http://127.0.0.1:8092";
+//const apiUrl = "http://127.0.0.1:8092";
+const apiUrl = "https://aigpt.wiut.uz";
 
 async function sendChat() {
     const chatLoading = document.getElementById("chatLoading");
